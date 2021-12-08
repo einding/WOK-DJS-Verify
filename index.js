01505355944e6281b6049100b45660d616de86ed
einding <64025460+einding@users.noreply.github.com>
2021-12-08 20:25:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ for (let i = 1; i < random1; i++){
 const random = Math.floor(Math.random() * 9)
 EndNumber = Endnumber + random
 }
-msg.channel.send("Hi! To verify it is you, please update the **ABOUT** section of your profile to following: " + EndNumber, {files: ["https://media.discordapp.net/attachments/848206920179515412/918218469642747945/Screenshot_20211208-201204_Chrome.jpg"])
+message.channel.send('Hi! To verify it is you, please update the **ABOUT** section of your profile to following: ' + EndNumber, {files: ["https://media.discordapp.net/attachments/848206920179515412/918218469642747945/Screenshot_20211208-201204_Chrome.jpg"])
 let blurb = await noblox.getBlurb({userId: UserId})
 
-msg.channel.send('Please check your ROBLOX messages!')
+message.channel.send('Please check your ROBLOX messages!')
 }
 startApp()
 })
