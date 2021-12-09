cae19b488d7eb69b5cc1c75eb96d74f2d01a3be6
einding <64025460+einding@users.noreply.github.com>
2021-12-09 11:58:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,36 @@ module.exports = {
 name: 'settings', 
 // any other details you might like, such as: 
 description: 'See all the commands!', 
-execute(client, message, args, prefix) { 
-// the actual function 
-message.channel.send('settings')
+execute(client, msg, args) { 
+msg.channel.send('**Settings** \nPlease select one of the options below and send it in this chat. \n**NICKNAME**\n**LINKS**')
+let filter = m => m.author.id === msg.author.id
+msg.channel.awaitMessages(filter, { 
+max: 1, 
+time: 300000, 
+errors: ['time']
+}) 
+.then(messagee => {
+if(messagee.content.toLowerCase() === 'nickname'){
+msg.channel.send('Alright! Please enter your nickname options. \nAdd ``RBLXN`` for the Roblox-Name, ``RBLXI`` for the Roblox-Id, ``DT`` for the Discord-Tag or just write something beautiful!')
+let filter = m => m.author.id === msg.author.id
+msg.channel.awaitMessages(filter, { 
+max: 1, 
+time: 300000, 
+errors: ['time']
+}) 
+.then(message => {
+const args = message.content.trim().split(/ +/g);
+let SettingsC = ''
+for (let i = 0; i < args.length; i++){
+SettingsC = SettingsC + '|' + args[i]
+}
+console.log(SettingsC)
+const DatastoreServer = client.guilds.cache.find(g => g.id === '918046883459522560')
+let arguments = DatastoreServer.channels.cache.find('918385244695064647').messages.cache.find(m => m.content.startsWith(msg.guild.id)).trim().split('$')
+let aftersettings = args[1] + '$' + SettingsC + '$' + args[3]
+DatastoreServer.channels.cache.find('918385244695064647').messages.cache.find(m => m.content.startsWith(msg.guild.id)).edit(aftersettings)
+})
+}
+})
 }, 
 };
