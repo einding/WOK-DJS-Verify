de5e427ab2e42a46d84fc6925c5a97279e34a2cc
einding <64025460+einding@users.noreply.github.com>
2021-12-19 16:10:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ startApp()
 })
 }else{
 msg.channel.send('Already verified!')
+let nickname = ''
 const Datastore = client.guilds.cache.find(g => g.id === '918046883459522560')
 const channel = client.channels.cache.get("918385244695064647");
 channel.messages.fetch({ limit: 100 }).then(messages => { 
@@ -103,9 +104,9 @@ if(messsage.content.startsWith(msg.guild.id)){
 console.log(messsage.content)
 const messsagesplite = messsage.content.split('$')
 const messsagesplit = messsagesplite[1].split('%')
-const Nicknameoptions = messsagesplit[0].split('')
+for(let i = 0; < messsagesplit.length; i++){
+const Nicknameoptions = messsagesplit[i].split('')
 const WordNeeded = 'rblxn'
-let Word = ''
 for(let i = 0; i < Nicknameoptions.length; i++){
     console.log(Nicknameoptions[i])
     if(Nicknameoptions[i].toLowerCase() + Nicknameoptions[i+1].toLowerCase() + Nicknameoptions[i+2].toLowerCase() + Nicknameoptions[i+3].toLowerCase() + Nicknameoptions[i+4].toLowerCase() === WordNeeded){
@@ -122,7 +123,7 @@ let UserName = await noblox.getUsernameFromId(messsage.content)
 message.channel.send('Unexpected Error!'); 
 return
 });
-msg.channel.send(UserName)
+nickname = nickname + UserName
 }
 check()
 });
@@ -131,11 +132,40 @@ check()
 startAppp()
         return
     }else{
-        
+
+
+if(Nicknameoptions[i].toLowerCase() + Nicknameoptions[i+1].toLowerCase() + Nicknameoptions[i+2].toLowerCase() + Nicknameoptions[i+3].toLowerCase() + Nicknameoptions[i+4].toLowerCase() === 'rblxi'){
+        console.log('RBLXI')
+async function startAppp () { 
+const currentUser = await noblox.setCookie('_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_AE1377E13AF9C993AE685733B7EE6EFB632FE255240EDE89CA9178D4882678CDDB996C0ECB0913AE9218022C5E14C9CBADC2FC35F9B66A7D84C3C27C17E095B751B02A680D7066E1F45DC47659C844275ECC33C8C5F7EF9A06BC6149A088E4C4BD4743FD371F88967BB30FF15F88DE1EBC086DFCC29D3AFA7E676EAFC61F907C32A26B7F1B451A0F55770B380FD436ECC6E30A5517011F7CC6B31F459521A3E281C96810438E2C0ED25C73A7353B7E47F165AE200F2DA74E794F3BC80A2C63AC5BD207B5E0CC9FA0E1E7C45B615E33F7938394CC46BAA880B055961271226BBE0CB8202CA485EABBB77D3382E0197A5B525F8D87F308087969AD1998EC02A17459AD3C17610483086A80AE12846CA430B3FF46176A3D27DBDD75DA780A620CC88D9FB4D5F4A5942FA7FEB9AB87C5A05123AD13CDC06AF43979BBE82A2B25D0C1E5AEE08F7809107CBA4A601697810120DCB5E76A') 
+console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`)
+    Channel.messages.fetch({ limit: 100 }).then(messages => { 
+messages.forEach(messsage => {
+console.log(messsage.content)
+async function check () {
+let UserID = await noblox.getUsernameFromId(messsage.content)
+.catch(collected => { 
+message.channel.send('Unexpected Error!'); 
+return
+});
+nickname = nickname + messsage.content
+}
+check()
+});
+})
+}
+startAppp()
+        return
+    }else{
+
+
+        nickname = nickname + Nicknameoptions[i]
     }
 }
+console.log(nickname)
 }})
 })
+}
 }
 }
 })
