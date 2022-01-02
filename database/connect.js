04300a44a0bc74e5c45039b97af3eaae46b28ddd
einding <64025460+einding@users.noreply.github.com>
2022-01-02 15:19:46 +0100

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,32 @@
+const firebaseConfig = {
+
+
+  apiKey: "AIzaSyCRhc0RdhVJ5Y6PSAIL_nEJjufiI9gGbZk",
+
+
+  authDomain: "verifydatabase-73acd.firebaseapp.com",
+
+
+  databaseURL: "https://verifydatabase-73acd-default-rtdb.europe-west1.firebasedatabase.app",
+
+
+  projectId: "verifydatabase-73acd",
+
+
+  storageBucket: "verifydatabase-73acd.appspot.com",
+
+
+  messagingSenderId: "423237483852",
+
+
+  appId: "1:423237483852:web:43d3b03efbd77f6a0623e9",
+
+
+  measurementId: "G-KRWJLETC0K"
+
+
+};
+
 const FirebaseAdmin = require("firebase-admin");
-FirebaseAdmin.initializeApp({ 
-credential: FirebaseAdmin.credential.cert(require("./accountAccess.json"))
-}, console.log(`Firebase');
+FirebaseAdmin.initializeApp(firebaseConfig);
 module.exports.db = FirebaseAdmin.firestore();
