44263d525374ab27becb8441f51348f8fa5adef4
einding <64025460+einding@users.noreply.github.com>
2022-01-02 15:11:40 +0100

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,1 +1,5 @@
-
+const FirebaseAdmin = require("firebase-admin");
+FirebaseAdmin.initializeApp({ 
+credential: FirebaseAdmin.credential.cert(require("./accountAccess.json"))
+}, console.log(`Firebase');
+module.exports.db = FirebaseAdmin.firestore();
